f86efdd3f8f2c8646261b8d1a85eb4cb8262ce4b
Oslo <eduardofabulam11@gmail.com>
2026-07-15 13:11:40 -0300

    Testes

---
+++ src/models/main/java/Alarme.java
@@ -0,0 +1,27 @@
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class Alarme {
+    private static Alarme instance = null; //Permite apenas criar um obj alarme, provalvelmente iremos trocar isso
+    //private Alarme alarme = new Alarme(); *Erro, coloquei criar obj direto fora de um metodo, fico se criando de forma infinita.
+
+    public static Alarme criarAlarme(){
+        if (instance == null){
+            instance = new Alarme();
+        }
+        return Alarme.instance;
+    }
+
+    public Duration addTempo(int hrs, int mins, int segs){ //validação das entradas vai ser por fora
+        Duration duration = Duration.ZERO; //pode ser refatorado para deixar melhor e mais legivel o codigo
+        duration = duration.plusHours(hrs).plusMinutes(mins).plusSeconds(segs);
+        return duration;
+    }
+
+    public LocalTime getHoraAtual(){
+        return LocalTime.now();
+    }
+    public String toString() {
+        return "";
+    }
+}
---
+++ src/models/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int hrs=0, mins=0, segs=0;
+        Alarme alarme = Alarme.criarAlarme();
+
+        LocalTime agora = alarme.getHoraAtual();
+
+
+        hrs = input.nextInt();
+        mins = input.nextInt();
+        segs = input.nextInt();
+        Duration duration = alarme.addTempo(hrs, mins, segs);
+
+        System.out.printf(agora.toString() + " Depois " + agora.plus(duration).toString());
+    }
+}
